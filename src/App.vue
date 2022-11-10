d5351718d20f54074ae8256734b46516200d7de3
sensiz <dev.federicosensi@gmail.com>
2022-11-10 15:14:51 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import SiteHeader from './components/SiteHeader.vue';
+
+export default {
+  components: {
+    name: 'SiteHeader',
+  }
+}
+</script>
+
+<template>
+
+  <header id="site-header"></header>
+  <!-- /#site-header -->
+  <main id="site_main"></main>
+  <!-- /#site_main -->
+  <footer id="site-footer"></footer>
+  <!-- /#site-footer -->
+
+</template>
+
+<style scoped>
+
+</style>
