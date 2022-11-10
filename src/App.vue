58e7e8bf97f2ec5d1a144acbebdc8c4851ac6980
sensiz <dev.federicosensi@gmail.com>
2022-11-10 15:48:57 +0100

    header complete

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,18 @@ import SiteHeader from './components/SiteHeader.vue';
 
 export default {
   components: {
-    name: 'SiteHeader',
+    SiteHeader,
   }
 }
 </script>
 
 <template>
 
-  <header id="site-header"></header>
+  <SiteHeader />
   <!-- /#site-header -->
   <main id="site_main"></main>
   <!-- /#site_main -->
-  <footer id="site-footer"></footer>
+  <footer id="site_footer"></footer>
   <!-- /#site-footer -->
 
 </template>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,9 +1,100 @@
-<script setup>
+<script>
+export default {
+    name: 'SiteHeader',
+    data() {
+        return {
+            links: [
+                {
+                    name: 'characters',
+                    link: '#'
+                },
+                {
+                    name: 'comics',
+                    link: '#'
+                },
+                {
+                    name: 'movies',
+                    link: '#'
+                },
+                {
+                    name: 'tv',
+                    link: '#'
+                },
+                {
+                    name: 'games',
+                    link: '#'
+                },
+                {
+                    name: 'collectibles',
+                    link: '#'
+                },
+                {
+                    name: 'videos',
+                    link: '#'
+                },
+                {
+                    name: 'fans',
+                    link: '#'
+                },
+                {
+                    name: 'news',
+                    link: '#'
+                },
+                {
+                    name: 'shop',
+                    link: '#'
+                },
+            ]
+        }
+    }
+}
 </script>
 
 <template>
+    <header id="site_header">
+        <div class="nav">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
+            <div class="links">
+                <a href="#" v-for="link in links">{{ link.name }}</a>
+            </div>
+        </div>
+        <div class="jumbotron">
+            <h1>--> Content goes here &lt-- </h1>
+        </div>
+    </header>
 </template>
 
-<style scoped>
+<style lang="scss">
+#site_header {
 
+    .nav {
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        padding: 1rem;
+
+        .links {
+
+            a {
+                padding: .5rem;
+                text-transform: uppercase;
+                text-decoration: none;
+                color: black;
+            }
+        }
+    }
+
+    .jumbotron {
+        background-color: black;
+        color: white;
+
+        h1 {
+            padding: 2rem;
+            text-align: center;
+        }
+    }
+
+}
 </style>
